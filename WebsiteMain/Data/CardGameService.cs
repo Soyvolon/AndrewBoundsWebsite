9f2c5a1abb44667751180df11eec3b818dfb2e2b
Andrew Bounds <andybounds@gmail.com>
2020-11-23 16:44:35 -0500

    Started building the Card Game Service

--- WebsiteMain/Data/CardGameService.cs
+++ WebsiteMain/Data/CardGameService.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using CardGameData;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using WebsiteMain.Database;
 
 namespace WebsiteMain.Data
 {
     public class CardGameService
     {
+        private readonly IServiceProvider _services;
+        public CardGame CurrentGame { get; private set; }
+        public CardGuess LastGuess { get; private set; }
+
+        public CardGameService(IServiceProvider serviceProvider)
+        {
+            _services = serviceProvider;
+            BuildInitalDataTree();
+        }
+
+        private async void BuildInitalDataTree()
+        {
+            var database = _services.GetRequiredService<AndrewBoundsDatabaseModel>();
+
+            var currentGame = database.CardGames.Where(x => x.EndDate == null);
+
+        }
+
 
     }
 }
